bd91963d3853f3753676dbb954a9f794000925a4
Graham Knop <haarg@haarg.org>
2016-10-31 06:31:10 -0400

    reindent

--- lib/Module/Reader.pm
+++ lib/Module/Reader.pm
@@ -15,94 +15,94 @@ use Scalar::Util qw(blessed reftype openhandle);
 use Carp;
 use constant _OPEN_STRING => $] >= 5.008;
 BEGIN {
-    require IO::String
-        if !_OPEN_STRING;
+  require IO::String
+    if !_OPEN_STRING;
 }
 
 sub module_content {
-    my $module = _get_module(@_);
-    if (ref $module) {
-        local $/;
-        return scalar <$module>;
-    }
-    else {
-        return $module;
-    }
+  my $module = _get_module(@_);
+  if (ref $module) {
+    local $/;
+    return scalar <$module>;
+  }
+  else {
+    return $module;
+  }
 }
 
 sub module_handle {
-    my $module = _get_module(@_);
-    if (ref $module) {
-        return $module;
-    }
-    elsif (_OPEN_STRING) {
-        open my $fh, '<', \$module;
-        return $fh;
-    }
-    else {
-        return IO::String->new($module);
-    }
+  my $module = _get_module(@_);
+  if (ref $module) {
+    return $module;
+  }
+  elsif (_OPEN_STRING) {
+    open my $fh, '<', \$module;
+    return $fh;
+  }
+  else {
+    return IO::String->new($module);
+  }
 }
 
 sub _get_module {
-    my ($package, @inc) = @_;
-    (my $module = "$package.pm") =~ s{::}{/}g;
-    my $opts = ref $_[-1] && ref $_[-1] eq 'HASH' && pop @inc || {};
-    if (!@inc) {
-        @inc = @INC;
+  my ($package, @inc) = @_;
+  (my $module = "$package.pm") =~ s{::}{/}g;
+  my $opts = ref $_[-1] && ref $_[-1] eq 'HASH' && pop @inc || {};
+  if (!@inc) {
+    @inc = @INC;
+  }
+  if (my $found = $opts->{found}) {
+    if (my $full_module = $found->{$module}) {
+      if (ref $full_module) {
+        @inc = $full_module;
+      }
+      elsif (-f $full_module) {
+        open my $fh, '<', $full_module
+          or die "Couldn't open ${full_module} for ${module}: $!";
+        return $fh;
+      }
     }
-    if (my $found = $opts->{found}) {
-        if (my $full_module = $found->{$module}) {
-            if (ref $full_module) {
-                @inc = $full_module;
-            }
-            elsif (-f $full_module) {
-                open my $fh, '<', $full_module
-                    or die "Couldn't open ${full_module} for ${module}: $!";
-                return $fh;
-            }
-        }
+  }
+  for my $inc (@inc) {
+    if (!ref $inc) {
+      my $full_module = File::Spec->catfile($inc, $module);
+      next unless -f $full_module;
+      open my $fh, '<', $full_module
+        or die "Couldn't open ${full_module} for ${module}: $!";
+      return $fh;
     }
-    for my $inc (@inc) {
-        if (!ref $inc) {
-            my $full_module = File::Spec->catfile($inc, $module);
-            next unless -f $full_module;
-            open my $fh, '<', $full_module
-                or die "Couldn't open ${full_module} for ${module}: $!";
-            return $fh;
-        }
 
-        my @cb = ref $inc eq 'ARRAY'  ? $inc->[0]->($inc, $module)
-               : blessed $inc         ? $inc->INC($module)
-                                      : $inc->($inc, $module);
+    my @cb = ref $inc eq 'ARRAY'  ? $inc->[0]->($inc, $module)
+           : blessed $inc         ? $inc->INC($module)
+                                  : $inc->($inc, $module);
 
-        next
-            unless ref $cb[0];
-        my $fh;
-        if (reftype $cb[0] eq 'GLOB' && openhandle $cb[0]) {
-            $fh = shift @cb;
-        }
-
-        if (ref $cb[0] eq 'CODE') {
-            my $cb = shift @cb;
-            # require docs are wrong, perl sends 0 as the first param
-            my @params = (0, @cb ? $cb[0] : ());
-
-            my $module = '';
-            while (1) {
-                local $_ = $fh ? <$fh> : '';
-                $_ = ''
-                    if !defined;
-                last if !$cb->(@params);
-                $module .= $_;
-            }
-            return $module;
-        }
-        elsif ($fh) {
-            return $fh;
-        }
+    next
+      unless ref $cb[0];
+    my $fh;
+    if (reftype $cb[0] eq 'GLOB' && openhandle $cb[0]) {
+      $fh = shift @cb;
     }
-    croak "Can't find module $module";
+
+    if (ref $cb[0] eq 'CODE') {
+      my $cb = shift @cb;
+      # require docs are wrong, perl sends 0 as the first param
+      my @params = (0, @cb ? $cb[0] : ());
+
+      my $module = '';
+      while (1) {
+        local $_ = $fh ? <$fh> : '';
+        $_ = ''
+          if !defined;
+        last if !$cb->(@params);
+        $module .= $_;
+      }
+      return $module;
+    }
+    elsif ($fh) {
+      return $fh;
+    }
+  }
+  croak "Can't find module $module";
 }
 
 1;
@@ -115,13 +115,13 @@ Module::Reader - Read the source of a module like perl does
 
 =head1 SYNOPSIS
 
-    use Module::Reader qw(:all);
-    my $io = module_handle('My::Module');
-    my $content = module_content('My::Module');
-    
-    my $io = module_handle('My::Module', @search_dirs);
-    
-    my $io = module_handle('My::Module', @search_dirs, { found => \%INC });
+  use Module::Reader qw(:all);
+  my $io = module_handle('My::Module');
+  my $content = module_content('My::Module');
+
+  my $io = module_handle('My::Module', @search_dirs);
+
+  my $io = module_handle('My::Module', @search_dirs, { found => \%INC });
 
 =head1 DESCRIPTION
 
--- t/main.t
+++ t/main.t
@@ -5,15 +5,15 @@ use Test::More 0.88;
 use Module::Reader qw(:all);
 
 my $mod_content = do {
-    open my $fh, '<', 't/lib/TestLib.pm';
-    local $/;
-    <$fh>;
+  open my $fh, '<', 't/lib/TestLib.pm';
+  local $/;
+  <$fh>;
 };
 
 {
-    local @INC = @INC;
-    unshift @INC, 't/lib';
-    is module_content('TestLib'), $mod_content, 'correctly load module from disk';
+  local @INC = @INC;
+  unshift @INC, 't/lib';
+  is module_content('TestLib'), $mod_content, 'correctly load module from disk';
 }
 
 done_testing;
