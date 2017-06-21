216b6879158c40e657e08b63063e5bc5a376289c
Graham Knop <haarg@haarg.org>
2017-06-21 12:21:25 -0400

    Bumping version to 0.003003

--- lib/Module/Reader.pm
+++ lib/Module/Reader.pm
@@ -3,7 +3,7 @@ BEGIN { require 5.006 }
 use strict;
 use warnings;
 
-our $VERSION = '0.003002';
+our $VERSION = '0.003003';
 $VERSION = eval $VERSION;
 
 use Exporter (); BEGIN { *import = \&Exporter::import }
